35c7547259156263c92e4bc9582f196958c6d90c
edenpuma33 <edenpuma33@gmail.com>
2025-02-14 13:30:56 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <button class="btn">
+        <span class="loading loading-spinner"></span>
+        loading
+      </button>
+    </>
+  );
+};
+export default App;
